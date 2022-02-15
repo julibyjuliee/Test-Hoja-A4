f4a569bf422c86eec4f0df0edcbc19ba1afaed47
Juliana <jurigo2003@gmail.com>
2022-02-15 11:26:31 -0500

    Complete test

--- JS/test.js
+++ JS/test.js
@@ -6,31 +6,30 @@ const CLOUDINARY_ID = "nri5jvre";
 
 function cloudinary() {
     subirImagen.addEventListener("change", async (e) => {
-    // console.log(e);
-    const file = e.target.files[0];
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", CLOUDINARY_ID);
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("upload_preset", CLOUDINARY_ID);
 
-    // Send to cloudianry
-    const res = await axios.post(CLOUDINARY_URL, formData, {
-        headers: {
-        "Content-Type": "multipart/form-data",
-    },
+        // Envio hacia cloudinary
+        const res = await axios.post(CLOUDINARY_URL, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        console.log(res);
+        visualizadorImg.src = res.data.secure_url;
     });
-    console.log(res);
-    visualizadorImg.src = res.data.secure_url;
-});
 }
 
 function validarExtension() {
     const subirImagen = document.getElementById("subirImagen").value;
     if (subirImagen != "") {
-    var verificar = subirImagen.toLowerCase();
-    if (!verificar.match(/(\.jpg|\.JPG)$/)) {
-        alert("Error: El formato no es compatible.");
-        document.getElementById("subirImagen").value = "";
+        var verificar = subirImagen.toLowerCase();
+        if (!verificar.match(/(\.jpg|\.JPG)$/)) {
+            swal("Error: El formato no es compatible.");
+            document.getElementById("subirImagen").value = "";
+        }
+        cloudinary();
     }
-    cloudinary();
-}
 }
