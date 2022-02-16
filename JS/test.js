56da60226877c9de1ae81d333b523d6647e32b29
Juliana <jurigo2003@gmail.com>
2022-02-16 15:14:46 -0500

    Cambios

--- JS/test.js
+++ JS/test.js
@@ -4,7 +4,6 @@ const subirImagen = document.getElementById("subirImagen");
 const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/dvi9x9ssf/image/upload`;
 const CLOUDINARY_ID = "nri5jvre";
 
-function cloudinary() {
     //Capturamos el evento 
     subirImagen.addEventListener("change", async (e) => {
 
@@ -23,16 +22,3 @@ function cloudinary() {
         console.log(res);
         visualizadorImg.src = res.data.secure_url;
     });
-}
-
-//Funcion que valida el formato de la extension 
-function validarExtension() {
-    const cargarImagen = document.getElementById("subirImagen").value;
-    if (cargarImagen != "") {
-        var verificar = cargarImagen.toLowerCase();
-        if (!verificar.match(/(\.jpg|\.JPG)$/)) {
-            swal("Error: El formato no es compatible.");
-        }
-        cloudinary();
-    }
-}
